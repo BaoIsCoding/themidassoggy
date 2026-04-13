9a47b2d46c86e1df643c841164aaa9ca8d7d3f23
Bao143 <taodiepgiabao070414@gmail.com>
2026-04-13 21:01:42 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,17 @@
 #include <Geode/modify/CCMenuItemSprite.hpp>
 
 class $modify(MyButtonHook, CCMenuItemSprite) {
-    void selected() {
-        CCMenuItemSprite::selected();
+void selected() {
+    CCMenuItemSprite::selected();
 
-        auto sprite = this->getNormalImage();
-        if (sprite) {
-            auto tex = CCTextureCache::sharedTextureCache()
-                ->addImage("soggy.png");
+    FMODAudioEngine::sharedEngine()->playEffect("honk.wav");
 
-            sprite->setTexture(tex);
-            sprite->setTextureRect(
-                CCRectMake(0, 0, tex->getContentSize().width, tex->getContentSize().height)
-            );
-        }
+    auto sprite = this->getNormalImage();
+    if (sprite) {
+        auto tex = CCTextureCache::sharedTextureCache()
+            ->addImage("soggy.png");
+
+        sprite->setTexture(tex);
     }
+}
 };

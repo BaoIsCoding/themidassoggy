b1e5ee760eac09e080637764c2fca69bc40cf210
Bao143 <taodiepgiabao070414@gmail.com>
2026-04-15 18:32:08 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ class $modify(MyButtonHook, CCMenuItemSprite) {
 
         if (auto sprite = typeinfo_cast<CCSprite*>(this->getNormalImage())) {
             auto texCache = CCTextureCache::sharedTextureCache();
-            auto newTexture = texCache->addImage("soggy.png");
+            auto newTexture = texCache->addImage("soggy.png", false);
 
             if (newTexture) {
                 sprite->setTexture(newTexture);

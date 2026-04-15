d3d82276d72b824497b0de5039c5266975e44288
Bao143 <taodiepgiabao070414@gmail.com>
2026-04-15 20:53:37 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,23 @@ using namespace geode::prelude;
 class $modify(CCMenuItemSpriteExtra) {
     void activate() {
         CCMenuItemSpriteExtra::activate();
+
         FMODAudioEngine::sharedEngine()->playEffect("honk.wav");
+
         auto texCache = CCTextureCache::sharedTextureCache();
         auto newTexture = texCache->addImage("soggycat.png", false);
 
         if (newTexture) {
-            if (auto sprite = static_cast<CCSprite*>(this->m_normalImage)) {
+            if (auto sprite = static_cast<CCSprite*>(this->getNormalImage())) {
                 sprite->setTexture(newTexture);
+
                 auto size = newTexture->getContentSize();
                 sprite->setTextureRect({ 0, 0, size.width, size.height });
+
                 this->updateSprite();
             }
         } else {
-            log::error("cant find soggy.png, check resources pls");
+            log::error("Could not find soggycat.png! Is it in your resources?");
         }
     }
 };

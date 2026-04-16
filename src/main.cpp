0ad37f15ad2f91b2687fae7ec5b5dc9ba32d92b2
Bao143 <taodiepgiabao070414@gmail.com>
2026-04-16 21:27:50 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@ class $modify(MyButtonHook, CCMenuItemSpriteExtra) {
 
     void selected() {
         CCMenuItemSpriteExtra::selected();
-        FMODAudioEngine::get()->playEffect("honk.wav");
-            if (auto spr = CCSprite::create("soggycat.png")) {
+        FMODAudioEngine::get()->playEffect("resource/honk.wav");
+            if (auto spr = CCSprite::create("resource/soggycat.png")) {
             this->setSprite(spr);
             this->updateSprite();
         }

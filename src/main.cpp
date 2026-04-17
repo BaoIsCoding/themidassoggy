6d1912c1ea552ed5c9080c8a66f33c36edc08b26
Bao143 <taodiepgiabao070414@gmail.com>
2026-04-17 20:50:26 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,23 @@
 #include <Geode/modify/CCMenuItemSpriteExtra.hpp>
-#include <Geode/binding/FMODAudioEngine.hpp>
 
 using namespace geode::prelude;
 
 class $modify(MyButtonHook, CCMenuItemSpriteExtra) {
-        void selected() {
-            CCMenuItemSpriteExtra::selected();
-                if (auto spr = CCSprite::create("soggycat.png"_spr)) {
-                this->setSprite(spr);
-                this->updateSprite();
+    void selected() {
+        CCMenuItemSpriteExtra::selected();
+        
+        auto currentSprite = this->getNormalImage();
+        if (currentSprite) {
+            auto newSpriteFrame = CCSpriteFrameCache::sharedSpriteFrameCache()->spriteFrameByName("soggycat.png"_spr);
+            
+            if (newSpriteFrame) {
+                static_cast<CCSprite*>(currentSprite)->setDisplayFrame(newSpriteFrame);
+            } else {
+                auto texture = CCTextureCache::sharedTextureCache()->addImage("soggycat.png"_spr);
+                if (texture) {
+                    static_cast<CCSprite*>(currentSprite)->setTexture(texture);
+                }
             }
         }
+    }
 };

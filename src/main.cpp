ee0024969c4afc507126a23958ba51ffd10a4f7d
Bao143 <taodiepgiabao070414@gmail.com>
2026-04-18 11:11:09 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,15 @@ using namespace geode::prelude;
 class $modify(MyButtonHook, CCMenuItemSpriteExtra) {
     void selected() {
         CCMenuItemSpriteExtra::selected();
-        
-        auto currentSprite = this->getNormalImage();
-        if (currentSprite) {
-            auto newSpriteFrame = CCSpriteFrameCache::sharedSpriteFrameCache()->spriteFrameByName("soggycat.png"_spr);
-            
-            if (newSpriteFrame) {
-                static_cast<CCSprite*>(currentSprite)->setDisplayFrame(newSpriteFrame);
-            } else {
-                auto texture = CCTextureCache::sharedTextureCache()->addImage("soggycat.png"_spr);
-                if (texture) {
-                    static_cast<CCSprite*>(currentSprite)->setTexture(texture);
-                }
+
+        if (auto sprite = this->getNormalImage()) {
+            if (auto tex = CCTextureCache::sharedTextureCache()->addImage("soggycat.png")) {
+                sprite->setTexture(tex);
+                sprite->setTextureRect(CCRectMake(
+                    0, 0,
+                    tex->getContentSize().width,
+                    tex->getContentSize().height
+                ));
             }
         }
     }
